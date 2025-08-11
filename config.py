3ea045453f46b46785187dd4c2d945600d8a55c9
bilalotw <bilalapple247@gmail.com>
2025-08-11 15:35:09 +0530

    updates

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/pothole_db")
-UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER", os.path.join(BASE_DIR, "uploads"))
+# Use MongoDB Atlas URI if provided in environment variables, otherwise fall back to local
+MONGO_URI = os.environ.get(
+    "MONGO_URI",
+    "mongodb+srv://bilalapple247:j6ooYDPj0aSvplT@cluster0.ujqltxa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+
+UPLOAD_FOLDER = os.environ.get(
+    "UPLOAD_FOLDER", os.path.join(BASE_DIR, "uploads"))
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "webp"}
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "change_this_secret_in_prod")
